ad66ab90f39fe8389f16d73f2d1c158b68364464
Robson Cruz <ghost0092@gmail.com>
2023-10-27 15:00:30 -0300

    docs(ioutils.go): add go doc comments to LoadEntries

--- internal/ioutils.go
+++ internal/ioutils.go
@@ -27,9 +27,15 @@ func SaveEntries(jounalEntries []JournalEntry) error {
 
 }
 
+// LoadEntries reads the JSON file with the given filename, unmarshals its contents
+// into a slice of JournalEntry, and returns the slice. If the file does not exists,
+// LoadEntries creates the file and returns an empty slice. If the file cannot be
+// created, LoadEntries returns an error.
 func LoadEntries(filename string) ([]JournalEntry, error) {
+	// Try to read the file
 	data, err := os.ReadFile(filename)
 	if err != nil {
+		// if the file does not exist, create it and return an empty slice
 		if errors.Is(err, os.ErrNotExist) {
 			_, err := os.Create(filename)
 			if err != nil {
@@ -37,9 +43,11 @@ func LoadEntries(filename string) ([]JournalEntry, error) {
 			}
 			return []JournalEntry{}, nil
 		}
+		// If there was another type of error, return the error
 		return nil, err
 	}
 
+	// Unmarshal JSON data into a slice of JournalEntry
 	var entries []JournalEntry
 	err = json.Unmarshal(data, &entries)
 	if err != nil {
