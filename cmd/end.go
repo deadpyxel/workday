5f5d84aedd78d5cce3f442b6e5359117cd240efc
Robson Cruz <ghost0092@gmail.com>
2023-10-30 13:23:17 -0300

    feat(cmd): add basic `end` command

--- cmd/end.go
+++ cmd/end.go
@@ -5,23 +5,49 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
+	journal "github.com/deadpyxel/workday/internal"
 	"github.com/spf13/cobra"
 )
 
 // endCmd represents the end command
 var endCmd = &cobra.Command{
 	Use:   "end",
-	Short: "A brief description of your command",
+	Short: "Marks the current workday as finished",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("end called")
-	},
+	RunE: markDayAsFinished,
+}
+
+func markDayAsFinished(cmd *cobra.Command, args []string) error {
+	// Get current date
+	now := time.Now()
+	currentDayId := now.Format("20060102")
+
+	// Load journal entries
+	entries, err := journal.LoadEntries("journal.json")
+	if err != nil {
+		return err
+	}
+
+	_, entryIndex := journal.FetchEntryByID(currentDayId, entries)
+
+	if entryIndex == -1 {
+		return fmt.Errorf("No entry found for the current day")
+	}
+
+	entries[entryIndex].EndDay()
+
+	err = journal.SaveEntries(entries)
+	if err != nil {
+		return fmt.Errorf("Failed to save journal entries: %v\n", err)
+	}
+	return nil
 }
 
 func init() {
