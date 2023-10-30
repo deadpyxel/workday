8973f74bc233d722a091a0349a4d5b4a8cb9c408
Robson Cruz <ghost0092@gmail.com>
2023-10-30 12:41:55 -0300

    test(journal): add test to FetchEntryByID
    some basic test cases to ensure the funciton contract is being kept

--- internal/journal_test.go
+++ internal/journal_test.go
@@ -3,10 +3,48 @@ package journal
 import "testing"
 
 func TestFindEntryByID(t *testing.T) {
-	entries := []JournalEntry{}
+	entryID := "0000"
+	entries := []JournalEntry{{ID: entryID}}
+	expectedEntry := entries[0]
+	testCases := []struct {
+		name       string
+		entries    []JournalEntry
+		expectedId string
+		expected   *JournalEntry
+	}{
+		{
+			name:       "When entry exists should return it",
+			entries:    entries,
+			expectedId: entryID,
+			expected:   &expectedEntry,
+		}, {
+			name:       "When entry does not exists should return nil",
+			entries:    entries,
+			expectedId: "9999",
+			expected:   nil,
+		}, {
+			name:       "When slice is empty should return nil",
+			entries:    []JournalEntry{},
+			expectedId: "1234",
+			expected:   nil,
+		}, {
+			name:       "When multiple entries returns correct entry",
+			entries:    append(entries, JournalEntry{ID: "1111"}),
+			expectedId: "0000",
+			expected:   &expectedEntry,
+		},
+	}
 
-	entry := FetchEntryByID("0000", entries)
-	if entry == nil {
-		t.Errorf("Expected an entry, got %v", entry)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			entry := FetchEntryByID(tc.expectedId, tc.entries)
+			if entry != nil && tc.expected != nil {
+				if entry.ID != tc.expected.ID {
+					t.Errorf("Expected %v, got %v", tc.expected, entry)
+				}
+			} else if entry != tc.expected {
+				t.Errorf("Expected %v, got %v", tc.expected, entry)
+			}
+		})
 	}
 }
