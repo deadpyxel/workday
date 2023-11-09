806e781544f0ed064714acbaf9e8f4bce472c0ed
Robson Cruz <ghost0092@gmail.com>
2023-11-09 19:41:04 -0300

    feat(report): add '`week` subcommand for report
    this subcommand will allow report generation on the whole week easier

--- cmd/report_week.go
+++ cmd/report_week.go
@@ -5,7 +5,6 @@ package cmd
 
 import (
 	"fmt"
-	"time"
 
 	journal "github.com/deadpyxel/workday/internal"
 	"github.com/spf13/cobra"
@@ -13,40 +12,38 @@ import (
 )
 
 // reportCmd represents the report command
-var reportCmd = &cobra.Command{
-	Use:   "report",
-	Short: "Reports the workday entry for the current day",
-	Long: `The report command is used to print out the workday entry for the current day.
-
-It loads the existing jorunal entries from the file and fetches the entry for the current day.
-If there is no entry for the current day, it returns an error.
-Otherwise, it prints out the entry.`,
-	RunE: reportWorkDay,
+var reportWeekCmd = &cobra.Command{
+	Use:   "week",
+	Short: "Generates a report for the current week",
+	Long: `The week command generates a report for the current week.
+It loads the existing journal entries from the file and fetches the entries for the current week.
+If there are no entries for the current week, it returns an error.
+Otherwise, it prints out the entries.`,
+	RunE: reportWeek,
 }
 
 // reportWorkDay reports the workday entry for the current day.
 // It first loads the existing journal entries from the file.
 // If there is no entry for the current day, it returns and error.
 // Otherwise, it prints out the entry.
-func reportWorkDay(cmd *cobra.Command, args []string) error {
+func reportWeek(cmd *cobra.Command, args []string) error {
 	journalPath := viper.GetString("journalPath")
 	journalEntries, err := journal.LoadEntries(journalPath)
 	if err != nil {
 		return err
 	}
-	now := time.Now()
-	currenctDayId := now.Format("20060102")
-	currentEntry, _ := journal.FetchEntryByID(currenctDayId, journalEntries)
-	if currentEntry == nil {
-		return fmt.Errorf("Could not find any entry for the current day.")
+	currentWeek, err := journal.CurrentWeekEntries(journalEntries)
+	if err != nil {
+		return err
 	}
-	fmt.Println(currentEntry)
+	for _, entry := range currentWeek {
+		fmt.Printf("%s---\n", entry.String())
+	}
 	return nil
 }
 
 func init() {
 	rootCmd.AddCommand(reportCmd)
-	reportCmd.AddCommand(reportWeekCmd)
 
 	// Here you will define your flags and configuration settings.
 
--- internal/journal.go
+++ internal/journal.go
@@ -58,3 +58,29 @@ func FetchEntryByID(id string, entries []JournalEntry) (*JournalEntry, int) {
 	}
 	return nil, -1
 }
+
+func CurrentWeekEntries(journalEntries []JournalEntry) ([]JournalEntry, error) {
+	if journalEntries == nil {
+		return []JournalEntry{}, fmt.Errorf("No entries were passed")
+	}
+	var currentWeekEntries []JournalEntry
+
+	// Get the current year and ISO week number.
+	now := time.Now()
+	currentYear, currentWeek := now.ISOWeek()
+
+	for _, entry := range journalEntries {
+		// get the year and ISO week number of the entry.
+		entryYear, entryWeek := entry.StartTime.ISOWeek()
+
+		// If the entry's year and week match the current year and week, add the entry to the slice.
+		if entryYear == currentYear && entryWeek == currentWeek {
+			currentWeekEntries = append(currentWeekEntries, entry)
+		}
+	}
+	if currentWeekEntries == nil {
+		return currentWeekEntries, fmt.Errorf("No entries found for the current week")
+	}
+
+	return currentWeekEntries, nil
+}
