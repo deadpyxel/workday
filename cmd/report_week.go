1f0a06fa4d2bc3f3ca88ad2dcd89bc932a2b7fae
Robson Cruz <ghost0092@gmail.com>
2023-11-10 17:13:52 -0300

    fix(cmd): remove duplicated call to `AddCommand`
    There was a second call adding the `report` command to the `rootCmd`, making duplicated entries
    appear on the help menu

--- cmd/report_week.go
+++ cmd/report_week.go
@@ -40,15 +40,5 @@ func reportWeek(cmd *cobra.Command, args []string) error {
 }
 
 func init() {
-	rootCmd.AddCommand(reportCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// reportCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// reportCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	reportCmd.AddCommand(reportWeekCmd)
 }
